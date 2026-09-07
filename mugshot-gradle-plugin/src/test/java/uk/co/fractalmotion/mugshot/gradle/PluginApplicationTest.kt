2555477f467867e19674af73b827e321b1d3e59b
Traz <traz.developer@gmail.com>
2026-09-07 19:11:15 +0100

    Order the File import as ktlint wants it

--- mugshot-gradle-plugin/src/test/java/uk/co/fractalmotion/mugshot/gradle/PluginApplicationTest.kt
+++ mugshot-gradle-plugin/src/test/java/uk/co/fractalmotion/mugshot/gradle/PluginApplicationTest.kt
@@ -1,8 +1,8 @@
 package uk.co.fractalmotion.mugshot.gradle
 
 import com.google.common.truth.Truth.assertThat
-import java.io.File
 import org.junit.Test
+import java.io.File
 
 /**
  * Applying the plugin to the module types and test frameworks it supports.
